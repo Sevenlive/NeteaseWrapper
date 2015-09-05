baab25882b2a776be793877ba6859d49cc794086
Sevenlive <lukaslange@live.de>
2015-09-05 14:55:15 +0200

    Get MP3_URL and other Stuff via Songid
    First Version on Github!

---
+++ songs.php
@@ -0,0 +1,20 @@
+<?php
+//set up variables
+$id = $_GET['id'];
+$url = "http://music.163.com/api/song/detail?ids=[{$id}]";
+//$data = array('s' => $_GET['s'], 'type' => $_GET['type'], 'offset' => $_GET['offset'], 'limit' => $_GET['limit']);
+$headers = "Referer: http://music.163.com\r\n".
+  "Content-Type: application/x-www-form-urlencoded\r\n".
+  "Host: music.163.com\r\n".
+  "Cookie: appver=2.0.2\r\n";
+$options = array(
+    'http' => array(
+        'header'  => $headers,
+        'method'  => 'GET',
+        'content' => '',
+    ),
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+echo($result);
+?>
